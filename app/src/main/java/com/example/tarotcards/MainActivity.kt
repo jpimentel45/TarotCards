218be1f884aa352ef06ca7eb05408cd791c6b9c4
juanito <jpimentel45@gmail.com>
2022-08-24 22:12:24 -0700

    Tarot Cards Room
    Signed-off-by: juanito <jpimentel45@gmail.com>

--- app/src/main/java/com/example/tarotcards/MainActivity.kt
+++ app/src/main/java/com/example/tarotcards/MainActivity.kt
@@ -1,21 +1,31 @@
 package com.example.tarotcards
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.example.tarotcards.repositories.TarotCardsRepository
 import com.example.tarotcards.tarot.TarotViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var viewModel : TarotViewModel
+    private lateinit var viewModel: TarotViewModel
+    private lateinit var repo: TarotCardsRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        repo = TarotCardsRepository(application)
         viewModel = ViewModelProvider(this)[TarotViewModel::class.java]
         viewModel.getTarotCards()
         val view = findViewById<TextView>(R.id.tv_hello)
         viewModel.list.observe(this){
             view.text = it.toString()
+            lifecycleScope.launch(Dispatchers.Unconfined) {
+                repo.insertAll(it ?: emptyList())
+
+            }
         }
     }
 }
